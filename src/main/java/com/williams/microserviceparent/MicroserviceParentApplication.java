d6f621256e248d8675a370f1f343a5b6add5a231
wsticks <akannimuyiwa@gmail.com>
2023-04-09 16:48:57 +0100

    Initial commit

---
+++ src/main/java/com/williams/microserviceparent/MicroserviceParentApplication.java
@@ -0,0 +1,13 @@
+package com.williams.microserviceparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceParentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/williams/microserviceparent/MicroserviceParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williams.microserviceparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
